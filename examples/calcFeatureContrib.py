f7ff72475ad21f728b0ed8a30c926060e6bab41d
Abdul Wahab <wahabjawed@gmail.com>
2020-06-25 01:51:21 +0200

    Organized Code for Readebility features, user controuition and quality of contribution

--- examples/calcFeatureContrib.py
+++ examples/calcFeatureContrib.py
@@ -1,50 +1,62 @@
-import glob
-import multiprocessing
-import os
-from itertools import islice
-
-import language_check
 import numpy
 import pandas as pd
-from textstat import textstat
-#import textstat
-
-from maya.nltk import util
-from readcalc import readcalc
 from mwapi import Session
+
 from maya.extractors import api
 
-def calcFeatures(index,params):
-    rev = params  # Multiprocessing...
+
+
+def calcQuality(params):
+    rev = params
+
+    if rev['rating'] == 'Stub':
+        quality_score = 0.1
+    elif rev['rating'] == 'Start':
+        quality_score = 0.28
+    elif rev['rating'] == 'C':
+        quality_score = 0.46
+    elif rev['rating'] == 'B':
+        quality_score = 0.64
+    elif rev['rating'] == 'GA':
+        quality_score = 0.82
+    elif rev['rating'] == 'FA':
+        quality_score = 1.0
+
+    return quality_score
+
+
+def calcFeatures(index, params):
+    rev = params
     global rev_xl
     global existingContrib
-    print(rev['revid'],'---',rev['userContrib'])
+    print(rev['revid'], '---', rev['userContrib'])
 
     if numpy.isnan(rev['userContrib']):
 
-        test = api_extractor.get_rev_doc_map([rev['revid']],{'ids', 'user','userid'})
+        test = api_extractor.get_rev_doc_map([rev['revid']], {'ids', 'user', 'userid', 'timestamp'})
 
         print(test)
         try:
             pageId = test[rev['revid']]['page']['pageid']
             userid = test[rev['revid']]['userid']
+            timestamp = test[rev['revid']]['timestamp']
             assert pageId == rev_xl.iloc[index, 0]
 
             pageid = rev['pageid']
-            print("page ID:", pageid,'--',index)
+            print("page ID:", pageid, '--', index)
             if pageid not in existingContrib:
                 values = api_extractor.get_all_revision_of_page_prop(pageid,
                                                                      rvprop={'ids', 'timestamp', 'size', 'userid'})
                 allUserContrib = dict()
                 totalContrib = 0
                 for item in values:
-                    for rev in item:
-                        if 'userid' in rev.keys():
-                            totalContrib += rev['size']
-                            if (rev['userid'] in allUserContrib):
-                                allUserContrib[rev['userid']] += rev['size']
+                    for revision in item:
+                        if 'userid' in revision.keys():
+                            totalContrib += revision['size']
+                            if (revision['userid'] in allUserContrib):
+                                allUserContrib[revision['userid']] += revision['size']
                             else:
-                                allUserContrib[rev['userid']] = rev['size']
+                                allUserContrib[revision['userid']] = revision['size']
 
                 userContrib = round(allUserContrib[userid] / totalContrib * 100, 2)
                 print(userContrib)
@@ -56,32 +68,34 @@ def calcFeatures(index,params):
                 re = existingContrib[pageid]
         except:
             re = 0.0
-        rev_xl.iloc[index, 35:36] = re
+        quality_score = calcQuality(rev)
+        contribQuality = round(quality_score * re, 2)
+        rev_xl.iloc[index, 35:40] = [quality_score, re, str(userid), str(timestamp), contribQuality]
+
+        print(rev_xl.iloc[index, :])
 
         if index % 10 == 0:
-            rev_xl.to_csv("readscore/all_score_train-c-output.csv")
-
-    return rev_xl.iloc[index, :]
-
-
+            rev_xl.to_csv(path)
 
 
 def startCalcFeatures():
     global rev_xl
 
-    #new_column = ['userContrib']
+    new_column = ['quality_score', 'userContrib', 'userID', 'timestamp', 'contribQuality']
 
-    rev_xl = rev_xl.reindex(columns=rev_xl.columns.tolist())
+    rev_xl = rev_xl.reindex(columns=rev_xl.columns.tolist() + new_column)
 
     # Perform classification.
     for index, row in rev_xl.iterrows():
         calcFeatures(index, row)
 
-    rev_xl.to_csv("readscore/all_score_train-c-output.csv")
+    rev_xl.to_csv(path)
 
 
 if __name__ == "__main__":
-    rev_xl = pd.read_csv("readscore/all_score_train-c-output.csv",
+    path = "readscore/all_score_train-c-output.csv"
+
+    rev_xl = pd.read_csv(path,
                          dtype={0: 'int32', 1: 'int32', 2: 'object'})
     session = Session("https://en.wikipedia.org/w/api.php", user_agent="test")
     api_extractor = api.Extractor(session)
--- examples/calcFeatureNew.py
+++ examples/calcFeatureNew.py
@@ -1,96 +1,68 @@
-import glob
-import multiprocessing
 import os
-from itertools import islice
-
 import language_check
 import pandas as pd
+from readcalc import readcalc
+from textstat.textstat import textstatistics
 
 from maya.nltk import util
-from readcalc import readcalc
 
 
 def calcFeatures(params):
     index, rev = params  # Multiprocessing...
     global rev_xl
-    filename = "/Users/abdulwahab/Desktop/internship/dataset/2015_english_wikipedia_quality_dataset/revisiondata/" + str(
+    filename = "insert data path of the 2015 data from https://figshare.com/articles/English_Wikipedia_Quality_Asssessment_Dataset/1375406" + str(
         rev['revid'])
     if (os.path.exists(filename)):
         print(rev['revid'])
         text = util.read_file(filename)
         text = util.cleanhtml(text)
+        text = text.replace('\'\'\'', '')
         assert rev['pageid'] == rev_xl.iloc[index, 0]
-        print("matched ",rev['revid'])
-        print(rev['revid'],'==cleaned')
-        # """
-        #     Returns a tuple with:
-        #      (number_chars, number_words, number_types, number_sentences, number_syllables, number_polysyllable_words,
-        #         number_words_longer_4, number_words_longer_6, number_words_longer_10,
-        #         number_words_longer_longer_13,
-        #          , lix_index, )
-        # """
+        print("matched ", rev['revid'])
 
-        #  linsear_write_formula, , readability_consensus, infonoisescore,
-        # logcontentlength, logreferences, logpagelinks, numimageslength, num_citetemplates, lognoncitetemplates,
-        # num_categories, hasinfobox, lvl2headings, lvl3heading
+        calc = readcalc.ReadCalc(text)
+        textual_score = list(calc.get_all_metrics())
 
-        # calc = readcalc.ReadCalc(text)
-        # t = calc.get_all_metrics()
+        text_stat = textstatistics()
+        linsear_write_formula = round(text_stat.linsear_write_formula(text),2)
+        textual_score.append(linsear_write_formula)
 
-        tool = language_check.LanguageTool('en-US')
-        text = u'A sentence with a error in the Hitchhiker’s Guide tot he Galaxy'
-        matches = tool.check(text)
-        len(matches)
+        grammar_score = len(tool.check(text))
+        textual_score.append(grammar_score)
 
-        tt = [round(util.check_grammar_error_rate_o(tool,text),2)]
-        print(rev['revid'], tt)
-        rev_xl.iloc[index, 34:35] = tt
+        rev_xl.iloc[index, 14:36] = textual_score
 
-        return rev_xl.iloc[index, :]
+        print(rev_xl.iloc[index, :])
 
-
-def clean():
-    rev_train = pd.read_csv("readscore/all_score_train-c-output.csv",
-                         dtype={0: 'int32', 1: 'int32', 2: 'object'})
-
-    rev_test = pd.read_csv("readscore/all_score_test-c-output.csv",
-                         dtype={0: 'int32', 1: 'int32', 2: 'object'})
-
-    print(rev_train.shape)
-    for index,row in rev_test.iterrows():
-        rev_train.drop(rev_train[rev_train.revid ==row['revid']].index, inplace=True)
-
-    print(rev_train.shape)
-
-    rev_train.to_csv("readscore/all_score_train-c-output.csv")
+        if index % 10 == 0:
+            rev_xl.to_csv(path)
 
 
 def startCalcFeatures():
     # Load rules from incredibly high-tech datastore.
 
-    # new_column = ['number_chars', 'number_words', 'number_types', 'number_sentences', 'number_syllables',
-    #               'number_polysyllable_words',
-    #               'difficult_words', 'number_words_longer_4', 'number_words_longer_6', 'number_words_longer_10',
-    #               'number_words_longer_longer_13', 'flesch_reading_ease', 'flesch_kincaid_grade_level',
-    #               'coleman_liau_index',
-    #               'gunning_fog_index', 'smog_index', 'ari_index', 'lix_index', 'dale_chall_score','linsear_write_formula']
+    new_column = ['number_chars', 'number_words', 'number_types', 'number_sentences', 'number_syllables',
+                  'number_polysyllable_words',
+                  'difficult_words', 'number_words_longer_4', 'number_words_longer_6', 'number_words_longer_10',
+                  'number_words_longer_longer_13', 'flesch_reading_ease', 'flesch_kincaid_grade_level',
+                  'coleman_liau_index',
+                  'gunning_fog_index', 'smog_index', 'ari_index', 'lix_index', 'dale_chall_score',
+                  'linsear_write_formula', 'grammar']
     global rev_xl
-    new_column = ['grammar']
 
     rev_xl = rev_xl.reindex(columns=rev_xl.columns.tolist() + new_column)
 
-    # calcFeatures([0,rev_xl.loc[rev_xl['article_revid'] == 491864508].squeeze()])
-
     # Perform classification.
-    with multiprocessing.Pool() as p:
-        result = p.map(calcFeatures, islice(rev_xl.iterrows(), 5))
-        #result = p.map(calcFeatures, rev_xl.iterrows())
+    for index, row in rev_xl.iterrows():
+        calcFeatures([index, row])
 
-    final = pd.DataFrame(data=result)
-    final.to_csv("readscore/all_score_train-c-output.csv")
+    rev_xl.to_csv(path)
 
 
 if __name__ == "__main__":
-    rev_xl = pd.read_csv("readscore/all_score_train-c-output.csv",
+    path = "readscore/all_score_train-c-output.csv"
+
+    rev_xl = pd.read_csv(path,
                          dtype={0: 'int32', 1: 'int32', 2: 'object'})
+    tool = language_check.LanguageTool('en-US')
     startCalcFeatures()

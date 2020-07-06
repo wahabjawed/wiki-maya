710ae79a77ddd4deb83f18ac339ec7324dc2ff38
Abdul Wahab <wahabjawed@gmail.com>
2020-07-06 11:43:27 +0200

    regression for more users

--- script/LongevityRegressionGraph.py
+++ script/LongevityRegressionGraph.py
@@ -29,42 +29,41 @@ def plotLongevityRegressionGraph():
     series1 = pd.DataFrame(data=data)
     series1 = series1[['pageid', 'timestamp', graph_for]]
     series1 = series1[series1.longevityRev >= 0]
+    series1['longevityRevN'] = series1['longevityRev'].shift(-1)
     series1['longevityRevP'] = series1['longevityRev'].shift(1)
+    series1 = series1[series1.longevityRevP >= 0]
+    series1 = series1[series1.longevityRevN >= 0]
     y = TrustScore([series1['longevityRev'], 24]).calculate()
 
-    with open('user_data/rev_list_39180130-dp.json', 'r') as infile:
-        data = json.loads(infile.read())
-    for d in data:
-        del d['next_rev']
+    arr1 = ['5834659','415269','39180130','9929111','29047545','36437087']
 
-    series2 = pd.DataFrame(data=data)
-    series2 = series2[['pageid', 'timestamp', graph_for]]
-    series2 = series2[series2.longevityRev >= 0]
-    series2['longevityRevP'] = series2['longevityRev'].shift(1)
-    y.extend(TrustScore([series2['longevityRev'], 24]).calculate())
+    for id in arr1:
 
-    with open('user_data/rev_list_415269-dp.json', 'r') as infile:
-        data = json.loads(infile.read())
-    for d in data:
-        del d['next_rev']
+        with open('user_data/rev_list_'+id+'-dp.json', 'r') as infile:
+            data = json.loads(infile.read())
+        for d in data:
+            del d['next_rev']
 
-    series3 = pd.DataFrame(data=data)
-    series3 = series3[['pageid', 'timestamp', graph_for]]
-    series3 = series3[series3.longevityRev >= 0]
-    series3['longevityRevP'] = series3['longevityRev'].shift(1)
+        series5 = pd.DataFrame(data=data)
+        series5 = series5[['pageid', 'timestamp', graph_for]]
+        series5 = series5[series5.longevityRev >= 0]
+        series5['longevityRevN'] = series5['longevityRev'].shift(-1)
+        series5['longevityRevP'] = series5['longevityRev'].shift(1)
+        series5 = series5[series5.longevityRevP >= 0]
+        series5 = series5[series5.longevityRevN >= 0]
+        y.extend(TrustScore([series5['longevityRev'], 24]).calculate())
 
-    y.extend(TrustScore([series3['longevityRev'], 24]).calculate())
+        series1 = series1.append(series5)
 
-    series1 = series1.append(series2)
-    series1 = series1.append(series3)
-    series1["longevityRevP"] = series1["longevityRevP"].fillna(0)
+    #series1["longevityRevP"] = series1["longevityRevP"].fillna(0)
+    #series1["longevityRevN"] = series1["longevityRevN"].fillna(0)
     series1["Trust"] = y
     series1['TrustP'] = series1['Trust'].shift(1)
     series1["TrustP"] = series1["TrustP"].fillna(0)
 
     X_train, X_test, y_train, y_test = train_test_split(
-        series1[['Trust', 'longevityRevP', 'TrustP']].values.reshape(-1, 3),
-        series1['longevityRev'].values.reshape(-1, 1), test_size=0.10, random_state=11)
+        series1[['Trust', 'longevityRev','longevityRevP']].values.reshape(-1, 3),
+        series1['longevityRevN'].values.reshape(-1, 1), test_size=0.10, random_state=17)
 
     regressor = LinearRegression()
     regressor.fit(X_train, y_train)
